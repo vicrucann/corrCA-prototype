59794eb0565d6fc9dd085798eb09f5fd0795d266
vicrucann <vicrucann@gmail.com>
2014-10-17 18:00:07 -0400

    Comments added

--- src/main_centering.cpp
+++ src/main_centering.cpp
@@ -1,3 +1,17 @@
+/* Chromatic aberration correction prototype.
+Copyright (C) 2014 Victoria Rudakova <vicrucann@gmail.com>
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "centers.h"
 #include "abberation.h"
 #include "distortion.h"
@@ -5,7 +19,6 @@
 #include "spline.h"
 #include "correction.h"
 
-#include "library.h"
 #include <cstring>
 #include <cstdio>
 #include <cstdlib>
